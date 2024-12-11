2c6893254fa4583e3c2e7c276a56e4483d5c6215
jeissonarcadio778 <jeissonbeltranporras@gmail.com>
2024-12-10 22:42:45 -0500

    feat: enhance getAllOrders function with filter expressions for date range and status

--- controllers/get-all-orders.js
+++ controllers/get-all-orders.js
@@ -12,38 +12,41 @@ const getAllOrders = async (event) => {
     let params = {
       TableName: 'CompletedOrdersTable',
     };
-
+    
     let filterExpressions = [];
     let expressionAttributeValues = {};
-
+    let expressionAttributeNames = {};
+    
     if (startDate || endDate) {
-      filterExpressions.push('createdAt BETWEEN :startDate AND :endDate');
+      filterExpressions.push('#createdAt BETWEEN :startDate AND :endDate');
       expressionAttributeValues[':startDate'] = startDate || '1970-01-01T00:00:00Z';
       expressionAttributeValues[':endDate'] = endDate || new Date().toISOString();
+      expressionAttributeNames['#createdAt'] = 'createdAt';
     }
-
+    
     if (status) {
-      filterExpressions.push('status = :status');
+      filterExpressions.push('#status = :status');
       expressionAttributeValues[':status'] = status;
+      expressionAttributeNames['#status'] = 'status';
     }
-
+    
     if (clientEmail) {
       filterExpressions.push('clientEmail = :clientEmail');
       expressionAttributeValues[':clientEmail'] = clientEmail;
     }
-
+    
     if (filterExpressions.length > 0) {
       params.FilterExpression = filterExpressions.join(' AND ');
       params.ExpressionAttributeValues = expressionAttributeValues;
+      params.ExpressionAttributeNames = expressionAttributeNames;
     }
-
+    
     console.log('Scan params:', params);
-
-    // Escanear la tabla
+    
     const result = await dynamodb.scan(params).promise();
-
+    
     const orders = result.Items.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
-
+    
     return {
       statusCode: 200,
       body: JSON.stringify({
@@ -51,7 +54,6 @@ const getAllOrders = async (event) => {
         orders,
       }),
     };
-
   } catch (error) {
     console.error('Error fetching orders:', error);
 
